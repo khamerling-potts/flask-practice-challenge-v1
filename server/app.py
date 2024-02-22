59850bef0d36a587f6874bac11b87aefd79814e4
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-02-22 11:05:24 -0500

    challenge 1 done

--- server/app.py
+++ server/app.py
@@ -2,8 +2,25 @@ from config import app, api
 from models import Post, Comment
 from flask_restful import Resource
 
-# create routes here:
 
+# create routes here:
+class SortedPosts(Resource):
+    def get(self):
+        posts = [post.to_dict() for post in Post.query.all()]
+        sorted_posts = sorted(posts, key=lambda post: post["title"])
+        return sorted_posts, 200
+
+
+class PostsByAuthor(Resource):
+    def get(self, name):
+        posts = [
+            post.to_dict() for post in Post.query.filter(Post.author == name).all()
+        ]
+        return posts, 200
+
+
+api.add_resource(SortedPosts, "/api/sorted_posts")
+api.add_resource(PostsByAuthor, "/api/posts_by_author/<author_name>")
 
 if __name__ == "__main__":
-  app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True)
