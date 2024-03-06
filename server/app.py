5919134d6c542ab89351013c6cf1abc7f70aa803
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-03-06 16:05:24 -0500

    added my own challenge

--- server/app.py
+++ server/app.py
@@ -47,11 +47,25 @@ class MostPopular(Resource):
         return {"commenter": max_commenter}, 200
 
 
+# Finds any posts where the given commenter commented on it
+class PostsWithCommenter(Resource):
+    def get(self, commenter):
+        result = []
+        posts = [post.to_dict() for post in Post.query.all()]
+        for post in posts:
+            commenters = [comment["commenter"] for comment in post["comments"]]
+            if commenters.count(commenter.title()):
+                result.append(post)
+
+        return result, 200
+
+
 api.add_resource(SortedPosts, "/api/sorted_posts")
 api.add_resource(PostsByAuthor, "/api/posts_by_author/<author_name>")
 api.add_resource(SearchPosts, "/api/search_posts/<title>")
 api.add_resource(PostsByComments, "/api/posts_ordered_by_comments")
-api.add_resource(MostPopular, "//api/most_popular_commenter")
+api.add_resource(MostPopular, "/api/most_popular_commenter")
+api.add_resource(PostsWithCommenter, "/api/posts_with_commenter/<commenter>")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
